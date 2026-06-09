2a0a7f91671db8717a13e3fa639e13261d58b03c
johnlatif19 <johnlatif17@gmail.com>
2026-06-10 02:55:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(express.static('.')); // يخدم الـ index.html
+app.use(express.static('.'));
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const CHAT_ID = process.env.CHAT_ID;
